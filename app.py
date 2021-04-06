13be7ef2a307f7dfff03711405c71f4f54e8bf13
Oleksandr Volkov <volkov.sawa@gmail.com>
2021-04-05 23:55:11 -0400

    Init DB and create Service page models

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
+from models.db import db
 
 
-app = Flask(__name__)
+from controllers.services_controller import ServicesController
+from controllers.service_controller import ServiceController
+
+
+# init app
+def initApp():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blmed.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
+
+
+app = initApp()
 api = Api(app)
 
-
-class Helo(Resource):
-    def get(self):
-        return {"data": "Hello"}
-
-
-api.add_resource(Helo, "/helo")
+# Routs
+api.add_resource(ServicesController, "/services")
+api.add_resource(ServiceController,
+                 "/service/<int:service_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
