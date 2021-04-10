a1b33d808b289374136b158ad235e6a139b7a204
Oleksandr Volkov <volkov.sawa@gmail.com>
2021-04-10 01:11:07 -0400

    Service delete by id - done

--- controllers/service_controller.py
+++ controllers/service_controller.py
@@ -1,14 +1,15 @@
 from flask_restful import Resource, abort
 from flask_apispec import marshal_with, doc
 from flask_apispec.views import MethodResource
-from models.service import ServiceModel
 
+from models.service import ServiceModel
 from models.service_schema import ServiceSchema
+from models.db import db
 
 
 # Controller for Service
-# Get services by id
 class ServiceController(MethodResource, Resource):
+    # Get services by id
     @doc(description='Get service by id', tags=['Services'])
     @marshal_with(ServiceSchema)
     def get(self, service_id):
@@ -16,3 +17,13 @@ class ServiceController(MethodResource, Resource):
             abort(404, message="Servise not found")
         result = ServiceModel.query.get(int(service_id))
         return result
+
+    # Delete service
+    @doc(description='Delete service by id', tags=['Services'])
+    def delete(self, service_id):
+        if ServiceModel.query.get(service_id) == None:
+            abort(404, message="Servise not found")
+        deleteServiceId = ServiceModel.query.get(int(service_id))
+        db.session.delete(deleteServiceId)
+        db.session.commit()
+        return "Service successfully deleted", 204
