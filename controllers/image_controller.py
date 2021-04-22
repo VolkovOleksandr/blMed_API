680abfa26b062b64ae1fb87df72329452d9814c8
Oleksandr Volkov <volkov.sawa@gmail.com>
2021-04-21 22:48:35 -0400

    Resize uploaded image is done

--- controllers/image_controller.py
+++ controllers/image_controller.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse, abort
 from flask_apispec.views import MethodResource
 from flask_apispec import doc
 from werkzeug.datastructures import FileStorage
+from PIL import Image
 import datetime
 
 # POST file field
@@ -25,11 +26,22 @@ class ImageController(MethodResource, Resource):
         for image in images_file:
             # Check if mime type of images acceptable
             if image.content_type == 'image/png' or image.content_type == 'image/jpeg' or image.content_type == 'image/heic':
+                # Prepearing name for image like unique ID
                 dateTimeForId = str(datetime.datetime.now())
                 imageId = dateTimeForId.replace(" ", "_")
                 mine = getMime(image.content_type)
                 imageName = imageId + mine
-                image.save('./images/{imageName}'.format(imageName=imageName))
+                # Resize and save image (max height or wigth 500px)
+                newImage = Image.open(image)
+                width, height = newImage.size
+                if height >= width:
+                    newImageResize = newImage.resize(
+                        (int((500*width)/height), 500))
+                else:
+                    newImageResize = newImage.resize(
+                        (500, int((500*height)/width)))
+                newImageResize.save(
+                    './images/{imageName}'.format(imageName=imageName))
                 imageIdArray.append(imageName)
             else:
                 abort(400, message="Image type should be .png .jpeg .heic")
